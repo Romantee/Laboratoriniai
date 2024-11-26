19a8b8112f9eac8e2be30a9a656d157630e018ab
Romantee <167504385+Romantee@users.noreply.github.com>
2024-11-26 14:57:42 +0200

    first commit

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+    const dropdown = document.getElementById("dropdownMenu");
+    dropdown.classList.toggle("show");
+}
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
