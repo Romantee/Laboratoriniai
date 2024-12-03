5708c09e8959cd50497b25c4476e2f59174ea320
Romantee <167504385+Romantee@users.noreply.github.com>
2024-12-03 15:40:39 +0200

    clock update

--- myscripts.js
+++ myscripts.js
@@ -34,3 +34,18 @@ function myFunction() {
   toggleScrollTop(); // Initial call
 });
 //-----------------------------------------------------------------------------
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
